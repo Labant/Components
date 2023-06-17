c56c1682ab2185a77947dde4c11e46c7db718941
Lvs <50939666+Labant@users.noreply.github.com>
2023-06-17 22:54:25 +0800

    Update LvsDrawGradientRound.hpp

--- DrawGtadientRound/LvsDrawGradientRound.hpp
+++ DrawGtadientRound/LvsDrawGradientRound.hpp
@@ -8,17 +8,13 @@
 
 class LvsDrawGradientRound:QObject
 {
-
 private:
     int radius = 120;
-
 public:
     explicit LvsDrawGradientRound(){};
     ~LvsDrawGradientRound(){};
 
-    void drawGradientRound(QImage& src,QPointF pos)
-    {
-
+    void drawGradientRound(QImage& src,QPointF pos){
     QPainter mPainter(&src);
     mPainter.setRenderHint(QPainter::Antialiasing,true);
 
@@ -30,16 +26,11 @@ public:
     mPainter.setPen(Qt::NoPen);
     mPainter.setBrush(QBrush(mRadialGradient));
     mPainter.drawEllipse(pos.rx() - this->radius,pos.ry() - this->radius,this->radius << 1,this->radius << 1);
-
     };
 
  //!setting
-    void setRadius(int R)
-    {
+    void setRadius(int R){
         this->radius = R;
     };
 };
-
-
-
 #endif
